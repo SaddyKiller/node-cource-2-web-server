7ebb53bdba3b093ccf40436bfcac8bf49b9daf87
Artem Melnik <artem.melnik@icloud.com>
2018-11-14 14:38:53 +0300

    Page titles change

--- server.js
+++ server.js
@@ -41,13 +41,13 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About page',
+    pageTitle: 'About',
   })
 })
 
 app.get('/projects', (req, res) => {
   res.render('projects.hbs', {
-    pageTitle: 'Projects page',
+    pageTitle: 'Projects',
   })
 })
 
